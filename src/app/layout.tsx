55406bd0dc771136d5612a2cefc6bb8c315ba1c0
Miraj Haq <mhaq@revcap.co.uk>
2025-10-01 16:36:42 +0100

    Name rebrand

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,8 +13,8 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Imposter",
-  description: "Made by Mir",
+  title: "Minposter",
+  description: "Imposter word game - Miraj",
 };
 
 export default function RootLayout({
